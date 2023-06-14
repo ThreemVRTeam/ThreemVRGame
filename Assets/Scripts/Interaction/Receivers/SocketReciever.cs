8d4a6506bbd7d6bd51e8b385af5e012ea36c0371
dwfdev <daniel.fraser.gamedesign@gmail.com>
2023-06-14 16:56:02 +1000

    Cord Sender/Receiver Start

---
+++ Assets/Scripts/Interaction/Receivers/SocketReciever.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Interaction
+{
+    public sealed class SocketReceiver : EventReceiver
+    {
+        EventSender cordSender;
+        public override void InitialiseComponents()
+        {
+
+        }
+
+        public override void Activate()
+        {
+            //cordSender.transform.parent;
+        }
+    }
+}
+
+namespace Interaction.CustomInspector
+{
+#if UNITY_EDITOR
+    using UnityEditor;
+
+    [CustomEditor(typeof(SocketReceiver)), Serializable]
+    public class SocketReceiverEditor : EventReceiverEditor { }
+#endif
+}
